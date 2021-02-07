1214fa03ec3d73091dd32227e9cc9aa859f63cf8
Rachel C <rcolli14@email.cpcc.edu>
2021-02-06 19:56:43 -0500

    Text added
    Program modified line added

--- MyFisrtProgram.java
+++ MyFisrtProgram.java
@@ -4,5 +4,6 @@ public class MyFisrtProgram
    public static void main(String[] args)
    {
       System.out.println("Hello world!");
+      System.out.println("Program modified, but able to revert to original version using GitHub Desktop.");
    }
 }
